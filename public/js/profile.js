d4a91eecf1d06bb181b3e99f1f1652f216d2394b
sfarias17 <sal.farias.job@gmail.com>
2021-05-22 12:55:34 -0600

    worked on image upload to cloudinary. added photo upload button

--- public/js/profile.js
+++ public/js/profile.js
@@ -32,6 +32,54 @@ const newFormHandler = async (event) => {
       alert('Failed to create dog');
     }
   }
+//   const CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/dr7w1oryg';
+// const CLOUDINARY_UPLOAD_PRESET = 'acbjtlov';
+// const image = document.querySelector('#fileupload');
+// image.addEventListener('change', (e) => {
+//   const file = e.target.files[0];
+//   const formData = new FormData();
+//   formData.append('file', file);
+//   formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
+
+//   fetch(CLOUDINARY_URL, {
+//     method: 'POST',
+//     body: formData,
+//   })
+//     .then(response => response.json())
+//     .then((data) => {
+//       if (data.secure_url !== '') {
+//         const uploadedFileUrl = data.secure_url;
+//         localStorage.setItem('passportUrl', uploadedFileUrl);
+//       }
+//     })
+//     .catch(err => console.error(err));
+// });
+const url = "https://api.cloudinary.com/v1_1/dr7w1oryg/upload";
+const form = document.querySelector("form");
+const CLOUDINARY_UPLOAD_PRESET = 'acbjtlov';
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const files = document.querySelector("[type=file]").files;
+  const formData = new FormData();
+
+  for (let i = 0; i < files.length; i++) {
+    let file = files[i];
+    formData.append("file", file);
+    formData.append("upload_preset", CLOUDINARY_UPLOAD_PRESET);
+
+    fetch(url, {
+      method: "POST",
+      body: formData
+    })
+      .then((response) => {
+        return response.text();
+      })
+      .then((data) => {
+        document.getElementById("data").innerHTML += data;
+      });
+  }
+});
 };
 
 const delButtonHandler = async (event) => {
